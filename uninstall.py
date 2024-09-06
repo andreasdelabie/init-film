e132733b8d2af9cc7adc22f9caba068fa5304834
AndreasDelabie <andreas.delabie@gmail.com>
2024-09-06 13:10:14 +0200

    Uninstaller also removes .exe file now

--- uninstall.py
+++ uninstall.py
@@ -5,11 +5,17 @@ pythonVersion = f'{sys.version_info.major}{sys.version_info.minor}'
 pythonPath = os.path.expanduser(f'~\\AppData\\Local\\Programs\\Python\\Python{pythonVersion}\\Scripts')
 print(f'Python version {pythonVersion} ({pythonPath})')
 
-scriptName = "init-film-script.py"
-scriptPath = os.path.join(pythonPath, scriptName)
+scriptName = "init-film"
+scriptFileName1 = scriptName+"-script.py"
+scriptFileName2 = scriptName+".exe"
+scriptPath1 = os.path.join(pythonPath, scriptFileName1)
+scriptPath2 = os.path.join(pythonPath, scriptFileName2)
 
-if os.path.exists(scriptPath):
-    os.remove(scriptPath)
-    print(f'Uninstalled {scriptName} from {scriptPath}')
+if os.path.exists(scriptPath1):
+    os.remove(scriptPath1)
+    print(f'Uninstalled {scriptName} from {scriptPath1}')
+elif os.path.exists(scriptPath2):
+    os.remove(scriptPath2)
+    print(f'Uninstalled {scriptName} from {scriptPath2}')
 else:
     print(f'{scriptName} not found in {pythonPath}')
