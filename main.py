cd0780eded7860615a380a38b65803b5cdaff955
AndreasDelabie <andreas.delabie@gmail.com>
2024-11-16 10:01:57 +0100

    Small documentation fault

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ osname = os.name
 
 def clearConsole():
     if osname == 'nt':
-        _ = os.system('cls') # For macOS & Linux
+        _ = os.system('cls') # For Windows
     else:
-        _ = os.system('clear') # For Windows
+        _ = os.system('clear') # For macOS & Linux
 
 
 def createFolder_level0():
