e2ed5647f8b1ddfab9738f886342f53a8d4af34e
andreasdelabie <andreas.delabie@gmail.com>
2025-08-08 19:54:36 +0200

    Annotated arg types where needed

--- src/initfilm/cli.py
+++ src/initfilm/cli.py
@@ -1,4 +1,5 @@
 import typer, initfilm
+from typing_extensions import Annotated
 from . import main, config, transcode
 
 
@@ -16,7 +17,10 @@ def default(context: typer.Context):
         main.createFolder_level0()
 
 @app.command()
-def create_proxies(folder_footage:str, codec: str = 'h264', resolution: str = '1280x720'):
+def create_proxies(
+    folder_footage:Annotated[str, typer.Option(help='Full path to footage folder.')],
+    codec:Annotated[str, typer.Option(help="Use 'h264', 'dnxhr', 'prores-proxy' or 'prores-lt'")]=config.get('proxies', 'default_codec'),
+    resolution:Annotated[str, typer.Option(help="Ex. '1280x720', '1920x1080', '3840x2160'")]=config.get('proxies', 'default_resolution')):
     """Create proxies for footage in the specified footage folder."""
     transcode.transcode_footage(folder_footage, codec, resolution)
 
